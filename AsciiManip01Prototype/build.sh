84a330e89f4bf5a6ad4c5105367707e767a96398
Shawn Mueller <smueller@syr.edu>
2013-03-02 15:23:25 -0500

    Fixed error in build script that did not statically link files

--- AsciiManip01Prototype/build.sh
+++ AsciiManip01Prototype/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gcc poc2.c -o a.out
-gcc poc2.c -s -o a.out.static
+gcc poc2.c -static -o a.out.static
 objdump -d a.out > a.out.objdump
 objdump -d a.out.static > a.out.static.objdump
 
